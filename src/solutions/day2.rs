e6eb6963bc9793f5f156a714978287055aa4ccdf
Peter Ivony <github.flask852@passmail.com>
2024-12-02 23:12:48 +0100

    Day 2 complete

--- src/solutions/day2.rs
+++ src/solutions/day2.rs
@@ -6,11 +6,57 @@ pub struct Day2 {
 }
 
 impl Solve for Day2 {
+
     fn solve_task_one(&self) -> Result<String, &'static str> {
-        Ok(String::new())
+        let safe_count = self.input.lines()
+        .filter(|line | {
+            let nums: Vec<i32> = line
+            .split_whitespace()
+            .map(|x| x.parse::<i32>().unwrap())
+            .collect();
+            safe_without_dampening(&nums)
+        })
+        .count();
+
+        Ok(safe_count.to_string())
     }
     
     fn solve_task_two(&self) -> Result<String, &'static str> {
-        Ok(String::new())
+        let safe_count = self.input.lines()
+        .filter(|line | {
+            let nums: Vec<i32> = line
+            .split_whitespace()
+            .map(|x| x.parse::<i32>().unwrap())
+            .collect();
+            safe_with_dampening(&nums)
+        })
+        .count();
+
+        Ok(safe_count.to_string())
+    }
+}
+
+pub fn safe_without_dampening(nums: &Vec<i32>) -> bool {
+        let is_increasing = nums.windows(2).all(|w| w[1] > w[0]);
+        let is_decreasing = nums.windows(2).all(|w| w[1] < w[0]);
+        
+        let valid_differences = nums.windows(2)
+            .all(|w| (w[1] - w[0]).abs() >= 1 && (w[1] - w[0]).abs() <= 3);
+        
+        (is_increasing || is_decreasing) && valid_differences
+}
+
+pub fn safe_with_dampening(nums: &Vec<i32>) -> bool {
+    if safe_without_dampening(nums) {
+        true
+    } else {
+        for i in 0..nums.len(){
+            let mut v = nums.clone();
+            v.remove(i);
+            if safe_without_dampening(&v) {
+                return true;
+            }
+        }
+        false
     }
 }
