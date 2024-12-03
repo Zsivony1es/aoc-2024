06ac59e3fda4cc6091f7b51949138dbda46fb331
Peter Ivony <github.flask852@passmail.com>
2024-12-03 23:46:06 +0100

    Day 3 complete

--- src/solutions.rs
+++ src/solutions.rs
@@ -1,5 +1,7 @@
 mod day1;
 mod day2;
+mod day3;
+mod day4;
 
 pub trait Solve {
     fn solve_task_one(&self) -> Result<String, &'static str>;
@@ -14,6 +16,7 @@ pub fn get_solver(day: &u8) -> Box<dyn Solve> {
     match day {
         1 => Box::new(day1::Day1{ day: day.clone(), input: read_input(day) }),
         2 => Box::new(day2::Day2{ day: day.clone(), input: read_input(day) }),
+        3 => Box::new(day3::Day3{ day: day.clone(), input: read_input(day) }),
         _ => panic!("No solver for day {}", day),
     }
 }
--- src/solutions/day3.rs
+++ src/solutions/day3.rs
@@ -1,5 +1,7 @@
 use super::Solve;
 
+use regex::{Match, Regex};
+
 pub struct Day3 {
     pub day: u8,
     pub input: String
@@ -7,10 +9,67 @@ pub struct Day3 {
 
 impl Solve for Day3 {
     fn solve_task_one(&self) -> Result<String, &'static str> {
-        Ok(String::new())
+        let re = Regex::new(r"mul\(\d+,\d+\)").unwrap();
+        let result = re
+            .find_iter(&self.input)
+            .fold(0, |acc, ma| {
+                let mystr: &str = ma.as_str();
+                let mystr: &str = &mystr[4..mystr.len() - 1];
+                acc + mystr
+                    .split(',')
+                    .map(|s| s.parse::<i32>().unwrap())
+                    .fold(1, |ac, x| ac * x)
+            });
+        Ok(result.to_string())
     }
     
     fn solve_task_two(&self) -> Result<String, &'static str> {
-        Ok(String::new())
+        let mul_re: Regex = Regex::new(r"mul\(\d+,\d+\)").unwrap();
+        let do_re: Regex = Regex::new(r"do(n't)?\(\)").unwrap();
+
+        let mut do_starts: Vec<usize> = do_re
+            .find_iter(&self.input)
+            .filter(|m| !m.as_str().contains("don't"))
+            .map(|m| m.start())
+            .collect::<Vec<usize>>();
+
+        do_starts.push(0);
+
+        let mut dont_starts: Vec<usize> = do_re
+            .find_iter(&self.input)
+            .filter(|m| m.as_str().contains("don't"))
+            .map(|m| m.start())
+            .collect::<Vec<usize>>();
+
+        do_starts.sort_unstable();
+        dont_starts.sort_unstable();
+
+        let result = mul_re
+            .find_iter(&self.input)
+            .fold(0, |acc, ma| {
+
+                let closest_do_start = do_starts
+                    .iter()
+                    .rev()
+                    .find(|&&x| x <= ma.start())
+                    .unwrap();
+
+                let closest_dont_start = dont_starts
+                    .iter()
+                    .rev()
+                    .find(|&&x| x <= ma.start());
+                
+                if closest_dont_start.is_none() || *closest_dont_start.unwrap() < *closest_do_start {
+                    let mystr: &str = ma.as_str();
+                    let mystr: &str = &mystr[4..mystr.len() - 1];
+                    acc + mystr
+                        .split(',')
+                        .map(|s| s.parse::<i32>().unwrap())
+                        .fold(1, |ac, x| ac * x)
+                } else {
+                    acc
+                }
+            });
+        Ok(result.to_string())
     }
 }
